ebea83efa8dd180d455911b476617915116babc1
Cosimo Streppone <cosimo@streppone.it>
2012-07-29 13:13:49 +0200

    We should be able to use vagrant ssh?

--- test.sh
+++ test.sh
@@ -7,9 +7,8 @@ if [[ -z "$DEPLOY_ONLY"  ]]; then
 fi
 
 echo "Password is 'vagrant'" &&
-(cat ~/.ssh/id_*.pub | ssh -p 2222 vagrant@localhost "cat > ./key; sudo mkdir /home/hdo/.ssh; sudo mv ./key /home/hdo/.ssh/authorized_keys; sudo chown -R hdo:hdo /home/hdo/.ssh") &&
-cd ../hdo-site &&
-bundle install &&
-cap deploy:setup deploy:migrations
-  
-    
+#(cat ~/.ssh/id_*.pub | ssh -p 2222 vagrant@localhost "cat > ./key; sudo mkdir /home/hdo/.ssh; sudo mv ./key /home/hdo/.ssh/authorized_keys; sudo chown -R hdo:hdo /home/hdo/.ssh") &&
+vagrant ssh -c "cat > ./key; sudo mkdir /home/hdo/.ssh; sudo mv ./key /home/hdo/.ssh/authorized_keys; sudo chown -R hdo:hdo /home/hdo/.ssh" && \
+    cd ../hdo-site && \
+    bundle install && \
+    cap deploy:setup deploy:migrations
