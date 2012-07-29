27a9bb44ba1fad5f9be671d65e46127db72e02d1
Jari Bakken <jari.bakken@gmail.com>
2012-07-30 01:19:12 +0200

    Log ruby::gem failures.

--- puppet/modules/ruby/manifests/init.pp
+++ puppet/modules/ruby/manifests/init.pp
@@ -20,8 +20,9 @@ class ruby {
 
 define ruby::gem {
   exec { "${name}-gem":
-    command => "gem1.9.1 install $name",
-    onlyif  => "gem1.9.1 search -i $name | grep false",
-    require => Package["ruby1.9.1"],
+    command   => "gem1.9.1 install $name",
+    onlyif    => "gem1.9.1 search -i $name | grep false",
+    require   => Package["ruby1.9.1"],
+    logoutput => on_failure
   }
 }
