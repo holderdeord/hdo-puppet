97405fac344539354d3533faabab58e485378565
Jari Bakken <jari.bakken@gmail.com>
2012-07-30 03:46:11 +0200

    Fix for setting Ruby 1.9 as the default Ruby version.

--- puppet/modules/ruby/manifests/init.pp
+++ puppet/modules/ruby/manifests/init.pp
@@ -10,11 +10,12 @@ class ruby {
       ensure => "installed";
   }
 
-  # Make the system-wide ruby link by default point at 1.9.1
-  file { "/usr/bin/ruby":
-    ensure  => "link",
-    target  => "/etc/alternatives/ruby",
-    require => Package['ruby1.9.1']
+  # Make sure the default Ruby is 1.9
+  exec { "update-alternatives-ruby-19":
+    command   => 'update-alternatives --set ruby /usr/bin/ruby1.9.1',
+    unless    => 'ruby -v | grep "ruby 1.9"',
+    require   => Package["ruby1.9.1"],
+    logoutput => on_failure
   }
 }
 
