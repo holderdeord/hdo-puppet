cae81d9fbf018cd03f902f3a37ff70f4d249ecc8
Jari Bakken <jari.bakken@gmail.com>
2012-08-03 11:21:26 +0200

    Trying out a postgresql module.

--- puppet/modules/hdo/manifests/database.pp
+++ puppet/modules/hdo/manifests/database.pp
@@ -2,23 +2,22 @@ class hdo::database {
 
   include hdo::common
 
-  # TODO Move out mysql users/passwords and DB creation from puppet?
+  # TODO Move out users/passwords and DB creation from puppet?
   #      maybe we should do this within the app deployment part...
 
-  if $mysql_root_password {} else { $mysql_root_password = "dont-use-this" }
-  if $mysql_hdo_password  {} else { $mysql_hdo_password  = "dont-use-this" }
+  if $postgresql_root_password {} else { $postgresql_root_password = "dont-use-this" }
+  if $postgresql_hdo_password  {} else { $postgresql_hdo_password  = "dont-use-this" }
 
-  class { "mysql::server":
+  class { 'postgresql::server':
     config_hash => {
-      "root_password" => $mysql_root_password,
-    }
+        'postgres_password' => $postgresql_root_password,
+    },
   }
 
-  mysql::db { "hdo_production":
-    user     => "hdo",
-    password => $mysql_hdo_password,
-    host     => "localhost",
-    grant    => ["all"],
+
+  postgresql::db { "hdo_production":
+    user => 'hdo',
+    password => postgresql_password('hdo', $postgresql_hdo_password)
   }
 
   file { "/home/hdo/.hdo-database.yml":
