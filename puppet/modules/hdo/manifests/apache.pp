3f7d092b4690984ccab2496a48933d448602849c
Jari Bakken <jari.bakken@gmail.com>
2012-09-01 00:45:46 +0200

    Extract apache to its own class + remove packages we no longer need.

---
+++ puppet/modules/hdo/manifests/apache.pp
@@ -0,0 +1,37 @@
+class hdo::apache {
+  include apache
+  include passenger
+  
+  # work around the apache module's dependency on docroot being present
+  # in reality, this is managed by capistrano deployments
+  file { '/webapps/hdo-site/current':
+    ensure  => link,
+    target  => '/webapps/hdo-site/tmp',
+    replace => false,
+    require => File['/webapps/hdo-site']
+  }
+
+  apache::vhost { 'beta.holderdeord.no':
+    vhost_name    => '*',
+    port          => 80,
+    priority      => '20',
+    servername    => 'beta.holderdeord.no',
+    serveradmin   => 'teknisk@holderdeord.no',
+    template      => 'hdo/vhost.conf.erb',
+    docroot       => '/webapps/hdo-site/current/public',
+    docroot_owner => 'hdo',
+    docroot_group => 'hdo',
+    options       => '-MultiViews -Indexes',
+    notify        => Service['httpd'],
+    require       => [A2mod['rewrite'], User['hdo']]
+  }
+
+  apache::vhost { 'files.holderdeord.no':
+    port        => 80,
+    priority    => '30',
+    servername  => 'files.holderdeord.no',
+    serveradmin => 'teknisk@holderdeord.no',
+    docroot     => '/webapps/files',
+    notify      => Service['httpd'],
+  }
+}
--- puppet/modules/hdo/manifests/backend.pp
+++ puppet/modules/hdo/manifests/backend.pp
@@ -1,10 +1,8 @@
 class hdo::backend {
 
   include hdo::common
-
-  include apache
   include ruby
-  include passenger
+  include hdo::apache
 
   $requirements = [
       'htop',
@@ -13,21 +11,18 @@ class hdo::backend {
       'libxml2',
       'libxml2-dev',
       'libxslt1-dev',
-      'git-core',
-      'libmysqlclient-dev',
       'libcurl4-openssl-dev',   # libcurl6?
       'imagemagick',
       'vim',
   ]
-
-  $serveradmin = 'teknisk@holderdeord.no'
 
   package { $requirements:
     ensure => 'installed'
   }
 
   ruby::gem { 'bundler':
-    name => 'bundler'
+    name => 'bundler',
+    version => '>= 1.2.0'
   }
 
   file { [ '/webapps', '/webapps/hdo-site', '/webapps/hdo-site/tmp' ]:
@@ -36,40 +31,8 @@ class hdo::backend {
     owner   => 'hdo'
   }
 
-  # work around the apache module's dependency on docroot being present
-  # in reality, this is managed by capistrano deployments
-  file { '/webapps/hdo-site/current':
-    ensure  => link,
-    target  => '/webapps/hdo-site/tmp',
-    replace => false
-  }
-
   a2mod { 'rewrite':
     ensure => present
-  }
-
-  apache::vhost { 'beta.holderdeord.no':
-    vhost_name    => '*',
-    port          => 80,
-    priority      => '20',
-    servername    => 'beta.holderdeord.no',
-    serveradmin   => $serveradmin,
-    template      => 'hdo/vhost.conf.erb',
-    docroot       => '/webapps/hdo-site/current/public',
-    docroot_owner => 'hdo',
-    docroot_group => 'hdo',
-    options       => '-MultiViews -Indexes',
-    notify        => Service['httpd'],
-    require       => [A2mod['rewrite'], User['hdo']]
-  }
-
-  apache::vhost { 'files.holderdeord.no':
-    port        => 80,
-    priority    => '30',
-    servername  => 'files.holderdeord.no',
-    serveradmin => $serveradmin,
-    docroot     => '/webapps/files',
-    notify      => Service['httpd'],
   }
 
 }
