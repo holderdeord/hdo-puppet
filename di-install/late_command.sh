e02647804683d8109e2d3f350de563afee60965d
Petter Reinholdtsen <pere@hungry.com>
2012-09-18 19:19:19 +0200

    First draft for autoconfiguration on a freshly installed machine.

--- di-install/late_command.sh
+++ di-install/late_command.sh
@@ -4,23 +4,28 @@
 echo "%sudo   ALL=NOPASSWD: ALL" >> /etc/sudoers
 
 # public ssh key for vagrant user
-mkdir /home/vagrant/.ssh
-wget -O /home/vagrant/.ssh/authorized_keys "https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub"
-chmod 755 /home/vagrant/.ssh
-chmod 644 /home/vagrant/.ssh/authorized_keys
-chown -R vagrant:vagrant /home/vagrant/.ssh
+if false; then
+    sshkeyurl="https://raw.github.com/mitchellh/vagrant/master/keys/vagrant.pub"
+    mkdir /home/vagrant/.ssh
+    wget -O /home/vagrant/.ssh/authorized_keys $sshkeyurl
+    chmod 755 /home/vagrant/.ssh
+    chmod 644 /home/vagrant/.ssh/authorized_keys
+    chown -R vagrant:vagrant /home/vagrant/.ssh
+fi
 
 # speed up ssh
 echo "UseDNS no" >> /etc/ssh/sshd_config
 
-# get chef
-#gem install chef --no-rdoc --no-ri
-
-apt-get install puppet
-
-# display login promt after boot
+# display login prompt after boot
 sed "s/quiet splash//" /etc/default/grub > /tmp/grub
 mv /tmp/grub /etc/default/grub
 update-grub
 
-apt-get clean
+(
+  cd /var/lib
+  git clone https://github.com/holderdeord/hdo-puppet/
+  (cd hdo-puppet ; git submodule update --init)
+  chown -R vagrant:vagrant hdo-puppet
+  puppet apply --modulepath=/var/lib/hdo-puppet/puppet/modules/ \
+      /var/lib/hdo-puppet/puppet/site.pp
+)
