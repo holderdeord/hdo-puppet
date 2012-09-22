98aa7d816c253f0e775053ed2e089b4ef10c9982
bjorn.dyresen@gmail.com <bjorn@Bjrn-Dyre-Dyresens-MacBook-Air.local>
2012-09-22 18:37:33 +0200

    using apache module to generate vhost

--- puppet/modules/munin/manifests/master.pp
+++ puppet/modules/munin/manifests/master.pp
@@ -3,7 +3,7 @@ class munin::master(
     $common_package = $munin::params::common_package_name,
     $package_ensure = "present") inherits munin::params {
 
-    require hdo::backend::apache
+    include apache
 
  
     #notice("Dette er i master")
@@ -22,14 +22,22 @@ class munin::master(
           ensure => present,
           content => template("munin/munin.conf")
       }
-      file {"/etc/apache2/sites-available/munin": 
-          ensure => present,
-          content => template("munin/apache.conf")
+
+      apache::vhost { 'munin.holderdeord.no':
+        vhost_name    => '*',
+        port          => 80,
+        priority      => '99',
+        servername    => 'munin.holderdeord.no',
+        serveradmin   => 'teknisk@holderdeord.no',
+        template      => 'munin/munin_vhost.conf.erb',
+        docroot       => $hdo::params::public_dir,
+        docroot_owner => 'hdo',
+        docroot_group => 'hdo',
+        options       => '-MultiViews -Indexes',
+        notify        => Service['httpd'],
+        #require       => [A2mod['rewrite'], User['hdo']]
       }
-      file {"/etc/apache2/sites-enabled/munin":
-          ensure => "../sites-available/munin",
-          require => File['/etc/apache2/sites-available/munin'],
-      }
+
     }
 
     config{"configfiles":
