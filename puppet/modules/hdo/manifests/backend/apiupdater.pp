fd52fbc0e556273ae300c5c05bfd11e80156f458
Jari Bakken <jari.bakken@gmail.com>
2012-10-05 00:00:41 +0200

    Add ability to disable apiupdater + use the rake task to trigger cache purge.

--- puppet/modules/hdo/manifests/backend/apiupdater.pp
+++ puppet/modules/hdo/manifests/backend/apiupdater.pp
@@ -2,8 +2,12 @@
 # this should only run on a single node!
 #
 
-class hdo::backend::apiupdater {
+class hdo::backend::apiupdater($ensure = present) {
   include hdo::params
+
+  if ! ($ensure in [ 'present', 'absent' ]) {
+    fail('apiupdater ensure parameter must be absent or present')
+  }
 
   $logfile = '/var/log/hdo-api-updater.log'
 
@@ -13,7 +17,8 @@ class hdo::backend::apiupdater {
   }
 
   cron { 'api-update':
-    command     => "cd ${hdo::params::app_root} && bundle exec script/import daily >> ${logfile} 2>&1",
+    ensure      => $ensure,
+    command     => "cd ${hdo::params::app_root} && bundle exec rake import:daily >> ${logfile} 2>&1",
     user        => hdo,
     environment => ['RAILS_ENV=production', 'PATH=/usr/local/bin:/usr/bin:/bin'],
     require     => [Class['hdo::backend'], File[$logfile]],
