ba4ed0a9faf611e957dbf6cd0091f97459a79573
Jari Bakken <jari.bakken@gmail.com>
2012-10-21 18:28:09 +0200

    Add hetzner02 node.

--- puppet/nodes.pp
+++ puppet/nodes.pp
@@ -1,9 +1,16 @@
 node 'default' {
-  include hdo::webapp::nginx
+  include hdo::webapp::apache
   include hdo::database
   # include munin::master
   # include munin::node
   # include elasticsearch
+}
+
+node 'hetzner02' {
+  include postfix
+  include hdo::webapp::nginx
+  include hdo::webapp::apiupdater
+  include hdo::database
 }
 
 node 'beta.holderdeord.no' {
