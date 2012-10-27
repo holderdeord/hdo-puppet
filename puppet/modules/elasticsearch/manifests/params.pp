efeaf791e87493233f2c6a5e501271dd8765fb56
Jari Bakken <jari.bakken@gmail.com>
2012-10-27 12:57:35 +0200

    Add wnorwegian as elasticsearch dependency (for word list).

--- puppet/modules/elasticsearch/manifests/params.pp
+++ puppet/modules/elasticsearch/manifests/params.pp
@@ -1,6 +1,8 @@
 class elasticsearch::params {
   $package = 'elasticsearch-0.19.9.deb'
-  $dependencies = ['java7-runtime-headless', 'java7-runtime' ]
-  #$java_home = '/usr/lib/jvm/java-1.7.0-openjdk-i386'
-  $download_to = '/root'
+  $dependencies = [
+    'java7-runtime-headless',
+    'java7-runtime',
+    'wnorwegian' # for word list
+  ]
 }
