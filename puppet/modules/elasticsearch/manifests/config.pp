6c53c8492bce1841ae4ef914c63429af4c40492f
Jari Bakken <jari.bakken@gmail.com>
2012-10-27 20:10:15 +0200

    Let's see if the default config files get memory management right.

--- puppet/modules/elasticsearch/manifests/config.pp
+++ puppet/modules/elasticsearch/manifests/config.pp
@@ -8,15 +8,15 @@ class elasticsearch::config inherits elasticsearch::params {
     content => template('elasticsearch/init.d/elasticsearch.erb')
   }
 
-  file { '/etc/elasticsearch/elasticsearch.yml':
-    ensure  => file,
-    mode    => '0644',
-    content => template('elasticsearch/elasticsearch.yml.erb')
-  }
-
-  file { '/etc/elasticsearch/logging.yml':
-    ensure  => file,
-    mode    => '0644',
-    content => template('elasticsearch/logging.yml.erb')
-  }
+  # file { '/etc/elasticsearch/elasticsearch.yml':
+  #   ensure  => file,
+  #   mode    => '0644',
+  #   content => template('elasticsearch/elasticsearch.yml.erb')
+  # }
+  # 
+  # file { '/etc/elasticsearch/logging.yml':
+  #   ensure  => file,
+  #   mode    => '0644',
+  #   content => template('elasticsearch/logging.yml.erb')
+  # }
 }
