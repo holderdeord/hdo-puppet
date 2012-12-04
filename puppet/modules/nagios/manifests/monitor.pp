60c6e6fe28f836e23399b01f5cc180f90635f750
root <root@ops1.holderdeord.no>
2012-12-04 18:30:13 +0100

    fjerner lint feil

--- puppet/modules/nagios/manifests/monitor.pp
+++ puppet/modules/nagios/manifests/monitor.pp
@@ -1,15 +1,15 @@
 # Manage the Nagios monitoring service
 class nagios::monitor {
   # Manage the packages
-  package { [ "nagios3", "nagios-plugins" ]: ensure => installed }
+  package { [ 'nagios3', 'nagios-plugins' ]: ensure => installed }
   # Manage the Nagios monitoring service
-  service { "nagios":
+  service { 'nagios':
     ensure    => running,
     hasstatus => true,
     enable    => true,
-    subscribe => [ Package["nagios3"], Package["nagios-plugins"] ],
+    subscribe => [ Package['nagios3'], Package['nagios-plugins'] ],
 }
   # collect resources and populate /etc/nagios/nagios_*.cfg
-  Nagios_host    <<||>> { notify => Service["nagios"] }
-  Nagios_service <<||>> { notify => Service["nagios"] }
+  Nagios_host    <<||>> { notify => Service['nagios'] }
+  Nagios_service <<||>> { notify => Service['nagios'] }
 }
