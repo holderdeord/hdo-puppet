d37e42e9ca9693e6d2fc68db9929ae46082ec69a
Jari Bakken <jari.bakken@gmail.com>
2012-12-06 18:13:11 +0100

    Remove wnorwegian package, not currently in use.

--- puppet/modules/elasticsearch/manifests/params.pp
+++ puppet/modules/elasticsearch/manifests/params.pp
@@ -2,8 +2,7 @@ class elasticsearch::params {
   $package = 'elasticsearch-0.19.9.deb'
   $dependencies = [
     'java7-runtime-headless',
-    'java7-runtime',
-    'wnorwegian' # for word list
+    'java7-runtime'
   ]
 
   $host         = '127.0.0.1' # default: 0.0.0.0
