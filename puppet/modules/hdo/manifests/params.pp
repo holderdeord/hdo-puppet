ca2060cd4456d84094d2ff7dcc6150cd9a1f98ca
Jari Bakken <jari.bakken@gmail.com>
2012-12-06 17:32:16 +0100

    Remove unused variables.

--- puppet/modules/hdo/manifests/params.pp
+++ puppet/modules/hdo/manifests/params.pp
@@ -8,15 +8,4 @@ class hdo::params {
   $deploy_root              = "${webapp_root}/hdo-site"
   $app_root                 = "${deploy_root}/current"
   $public_dir               = "${app_root}/public"
-
-  $unicorn_conf             = '/etc/unicorn.hdo.conf'
-
-  $unicorn_run_dir          = '/var/run/unicorn'
-  $unicorn_log_dir          = '/var/log/unicorn'
-
-  $unicorn_socket           = "${unicorn_run_dir}/hdo.sock"
-  $unicorn_pid              = "${unicorn_run_dir}/hdo.pid"
-  $unicorn_log              = "${unicorn_log_dir}/hdo.log"
-
-  $unicorn_worker_processes = '4'
 }
