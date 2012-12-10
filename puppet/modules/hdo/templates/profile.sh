e9405657c52a20ed4ef06fb4ddb7c5a15a654f41
Jari Bakken <jari.bakken@gmail.com>
2012-12-10 22:16:36 +0100

    Fix typo.

--- puppet/modules/hdo/templates/profile.sh
+++ puppet/modules/hdo/templates/profile.sh
@@ -3,5 +3,5 @@
 #
 
 alias hdo-current='cd <%= scope.lookupvar("hdo::params::app_root") %>'
-alias hdo-tail='tail -500f <%= scope.lookupvar("hdo::params::app_root") %>/log/<%= scope.lookupvar("hdo::params::enviroment") %>.log'
+alias hdo-tail='tail -500f <%= scope.lookupvar("hdo::params::app_root") %>/log/<%= scope.lookupvar("hdo::params::environment") %>.log'
 alias hdo-console='cd <%= scope.lookupvar("hdo::params::app_root") %> && RAILS_ENV=<%= scope.lookupvar("hdo::params::environment") %> bundle exec rails console'
