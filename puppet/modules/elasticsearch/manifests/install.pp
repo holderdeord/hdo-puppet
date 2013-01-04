be38a25eacda9abebe93dae130ae1bfda10aaf8c
Jari Bakken <jari@holderdeord.no>
2013-01-04 01:26:48 +0100

    elasticsearch is no longer hosted on GitHub

--- puppet/modules/elasticsearch/manifests/install.pp
+++ puppet/modules/elasticsearch/manifests/install.pp
@@ -4,7 +4,7 @@ class elasticsearch::install(
 
   exec {'elasticsearch-download':
     # need to depend on wget?
-    command => "wget https://github.com/downloads/elasticsearch/elasticsearch/${elastic_package} -O /root/${elastic_package}",
+    command => "wget http://download.elasticsearch.org/elasticsearch/elasticsearch/${elastic_package} -O /root/${elastic_package}",
     creates => "/root/${elastic_package}",
   }
 
