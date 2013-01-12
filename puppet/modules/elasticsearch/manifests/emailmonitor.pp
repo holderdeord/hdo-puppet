a87a42b54c6b92e7776fd307e32fff32e85c3739
Bjørn Dyresen <bjorn.dyresen@gmail.com>
2013-01-12 13:36:49 +0100

    Sjekker for 200 svar. Ut med cronsjekk.

--- puppet/modules/elasticsearch/manifests/emailmonitor.pp
+++ puppet/modules/elasticsearch/manifests/emailmonitor.pp
@@ -1,5 +1,5 @@
 # Cron+email check of ES until we have proper monitoring
-class elasticsearch::emailmonitor($ensure = present) {
+class elasticsearch::emailmonitor($ensure = absent) {
   cron { 'elasticsearch-email':
     ensure      => $ensure,
     command     => 'nc -z localhost 9200 >/dev/null 2>&1 || echo "elasticsearch not responding"',
