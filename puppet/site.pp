57dea4c4d5a029b869b88fe5b3b2c4c09154078d
Jari Bakken <jari.bakken@gmail.com>
2013-03-02 04:07:33 +0100

    More exec defaults.

--- puppet/site.pp
+++ puppet/site.pp
@@ -2,7 +2,15 @@ exec { 'apt-update':
   command => '/usr/bin/apt-get update',
 }
 
-Exec { path => '/usr/bin:/bin:/usr/local/bin' }
+Exec {
+  logoutput => on_failure,
+  path => [
+    '/usr/bin',
+    '/bin',
+    '/usr/local/bin'
+  ]
+}
+
 Exec['apt-update'] -> Package <| |>
 
 import 'nodes.pp'
