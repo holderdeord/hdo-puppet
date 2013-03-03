2cf8c33657efb8d6c399fbfcd6fc73db80e63241
Jari Bakken <jari@holderdeord.no>
2013-03-03 05:02:14 +0100

    Bump elasticsearch to 0.20.5.
    Can't stay on 0.20.2 after reading https://github.com/blog/1397-recent-code-search-outages

--- modules/elasticsearch/manifests/params.pp
+++ modules/elasticsearch/manifests/params.pp
@@ -1,5 +1,5 @@
 class elasticsearch::params {
-  $package = 'elasticsearch-0.20.2.deb'
+  $package = 'elasticsearch-0.20.5.deb'
   $dependencies = [
     'java7-runtime-headless',
     'java7-runtime'
