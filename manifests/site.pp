f6499bb866f79208ffc65545e8c86974b44c1511
Jari Bakken <jari@holderdeord.no>
2013-04-27 21:17:07 +0200

    Use a custom sources.list
    The default from Hetzner's image points at outdated mirrors.

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,18 @@
+#
+# Hetzner's default Ubuntu image points at their outdated mirrors, so we add our own sources.list here.
+#
+
+file { "/etc/apt/sources.list":
+  ensure => file,
+  source => "puppet:///modules/hdo/apt/sources.list",
+  owner  => root,
+  group  => root,
+  mode   => '0644',
+}
+
 exec { 'apt-update':
   command => '/usr/bin/apt-get update',
+  require => File['/etc/apt/sources.list']
 }
 
 Exec {
