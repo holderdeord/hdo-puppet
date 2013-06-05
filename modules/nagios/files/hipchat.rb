eedbb5b4aea09726eb72c756e66a3918a39fef7b
Jari Bakken <jari.bakken@gmail.com>
2013-06-05 22:48:12 +0200

    fix shebang + add input parsing

--- modules/nagios/files/hipchat.rb
+++ modules/nagios/files/hipchat.rb
@@ -1,4 +1,4 @@
-#!/usr/bin/ruby
+#!/usr/bin/env ruby
 
 require 'hipchat'
 require 'optparse'
@@ -22,6 +22,17 @@ if options.values.any?(&:nil?)
   puts "invalid options: #{options.inspect}"
   exit 1
 end
+
+case options[:type]
+when 'host'
+  hostname, long_date_time, notification_type, host_address, host_state, host_output = options[:inputs].split('|')
+when 'service'
+  service_desc, host_alias, long_date_time, notification_type, host_address, service_state, service_output = options[:inputs].split('|')
+else
+  exit 1
+end
+
+
 
 client = HipChat::Client.new(options[:token])
 client[options[:room]].send('Nagios', "Test: #{options[:inputs].inspect} | Type: #{options[:type]}")
