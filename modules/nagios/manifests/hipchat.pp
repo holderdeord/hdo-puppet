9af83cec4261a7c6450516bd2ef93e7f9f91bd8d
Jari Bakken <jari.bakken@gmail.com>
2013-06-06 00:39:44 +0200

    Make sure nagios can execute the script.

--- modules/nagios/manifests/hipchat.pp
+++ modules/nagios/manifests/hipchat.pp
@@ -3,7 +3,8 @@ class nagios::hipchat {
 
   file { '/etc/nagios-plugins/hipchat.rb':
     ensure  => file,
-    mode    => '0744',
+    mode    => '0774',
+    owner   => 'nagios',
     group   => 'nagios',
     source  => 'puppet:///modules/nagios/hipchat.rb',
     require => Package['nagios-plugins']
