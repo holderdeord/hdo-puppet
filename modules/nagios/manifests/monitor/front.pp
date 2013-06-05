accd91abaf87f334d79347bd80fbbf5b3e62fa2b
Jari Bakken <jari.bakken@gmail.com>
2013-06-05 14:42:14 +0200

    Fix nagios::monitor::front to actually create host definitions.

--- modules/nagios/manifests/monitor/front.pp
+++ modules/nagios/manifests/monitor/front.pp
@@ -1,8 +1,28 @@
 class nagios::monitor::front {
+  $front_hosts = '/etc/nagios3/conf.d/front_hosts.cfg'
+
+  file { $front_hosts:
+    ensure => file,
+    owner  => root,
+    mode   => '0644',
+  }
+
+  nagios_host { 'www.holderdeord.no':
+    ensure  => present,
+    use     => 'generic-host',
+    target  => $front_hosts,
+  }
+
+  nagios_host { 'holderdeord.no':
+    ensure => present,
+    use    => 'generic-host',
+    target => $front_hosts,
+  }
+
   nagios_service { 'check_http_holderdeord.no':
     check_command       => 'check_http!holderdeord.no',
     use                 => 'generic-service',
-    host_name           => 'localhost',
+    host_name           => 'holderdeord.no',
     notification_period => '24x7',
     service_description => 'holderdeord_check_http',
   }
@@ -10,7 +30,7 @@ class nagios::monitor::front {
   nagios_service { 'check_http_www.holderdeord.no':
     check_command       => 'check_http!www.holderdeord.no',
     use                 => 'generic-service',
-    host_name           => 'localhost',
+    host_name           => 'www.holderdeord.no',
     notification_period => '24x7',
     service_description => 'www_check_http',
   }
