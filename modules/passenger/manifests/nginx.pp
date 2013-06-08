83383a89b5bc73f65433f3f7e4f13ca1660380a3
Jari Bakken <jari.bakken@gmail.com>
2013-06-08 17:29:55 -0400

    Remove duplicated exec defaults.

--- modules/passenger/manifests/nginx.pp
+++ modules/passenger/manifests/nginx.pp
@@ -10,11 +10,9 @@ class passenger::nginx($port = 80) inherits passenger {
   $listen      = $port
 
   exec { 'install-passenger-nginx':
-    path      => ['/bin', '/usr/bin', '/usr/local/bin'],
     command   => "passenger-install-nginx-module --extra-configure-flags='--with-http_stub_status_module' --auto --auto-download --prefix ${root}",
     creates   => $root,
     require   => [Ruby::Gem['passenger'], Package['libcurl4-openssl-dev']],
-    logoutput => on_failure,
     timeout   => 600
   }
 
