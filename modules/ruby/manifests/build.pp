50bdbf392bcecf2c654e800b8d690012e13f59e8
Jari Bakken <jari@holderdeord.no>
2013-11-23 12:29:20 +0100

    Make sure ruby-build is always up to date

--- modules/ruby/manifests/build.pp
+++ modules/ruby/manifests/build.pp
@@ -1,9 +1,15 @@
 class ruby::build {
-  $download = '/tmp/ruby-build'
+  $dir = '/opt/ruby-build'
 
-  exec { 'install-ruby-build':
-    command => "git clone git://github.com/sstephenson/ruby-build ${download} && cd ${download} && ./install.sh",
-    creates => '/usr/local/bin/ruby-build',
+  exec { 'clone-ruby-build':
+    command => "git clone git://github.com/sstephenson/ruby-build ${dir} && cd ${dir}",
+    creates => $dir,
     require => Package['git-core'],
+  }
+
+  exec { 'update-ruby-build':
+    command => 'git reset --hard origin/master && git pull && ./install.sh',
+    cwd     => $dir,
+    require => Exec['clone-ruby-build']
   }
 }
