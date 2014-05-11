435f8097052e29de85bf0f8eeeb9e368dfa8f290
Jari Bakken <jari@holderdeord.no>
2014-05-11 13:26:07 +0200

    Remove redundant timestamps

--- modules/hdo/manifests/webapp/exporter.pp
+++ modules/hdo/manifests/webapp/exporter.pp
@@ -13,7 +13,7 @@ class hdo::webapp::exporter(
 
   cron { 'data-export':
     ensure      => $ensure,
-    command     => "bash -l -c 'cd ${hdo::params::app_root} && bundle exec script/export /tmp/hdo-csv-dump-$(date +%F) && tar zcf /webapps/files/data/csv/latest.tgz -C /tmp/ hdo-csv-dump-$(date +%F)'",
+    command     => "bash -l -c 'cd ${hdo::params::app_root} && bundle exec script/export /tmp/hdo-csv-dump && tar zcf /webapps/files/data/csv/latest.tgz -C /tmp/ hdo-csv-dump'",
     user        => postgres,
     environment => ["RAILS_ENV=${hdo::params::environment}", 'PATH=/usr/local/bin:/usr/bin:/bin', "MAILTO=${hdo::params::admin_email}"],
     require     => Class['hdo::webapp'],
