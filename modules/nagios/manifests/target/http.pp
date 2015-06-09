890fd9a40c90809c9bb941bd832541cebb847eb5
Jari Bakken <jari@holderdeord.no>
2015-06-09 23:57:22 +0200

    Authorization for nagios check

--- modules/nagios/manifests/target/http.pp
+++ modules/nagios/manifests/target/http.pp
@@ -1,6 +1,16 @@
-class nagios::target::http {
-  @@nagios_service { "check_http_${::hostname}":
-    check_command          => "check_http!${::ipaddress}",
+class nagios::target::http($auth = undef) {
+
+  $command       = "check_http_${::hostname}"
+  $check_command = "check_http!${::ipaddress}"
+
+  if $auth != undef {
+    $command       = "check_http_auth_${::hostname}"
+    $authParts     = split($auth, ' ')
+    $check_command = "check_http_auth!${::ipaddress}!${authParts[0]}!${authParts[1]}"
+  }
+
+  @@nagios_service { $command:
+    check_command          => $check_command,
     check_interval         => '1',
     use                    => 'generic-service',
     host_name              => $::fqdn,
