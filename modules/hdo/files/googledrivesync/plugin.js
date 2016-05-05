c995950d3c77ae92d72ef98791133ddd5b8ead9e
Jari Bakken <jari@holderdeord.no>
2016-05-05 21:43:44 +0200

    We don't cache static files, so this isn't necessary.

--- modules/hdo/files/googledrivesync/plugin.js
+++ modules/hdo/files/googledrivesync/plugin.js
@@ -2,11 +2,11 @@ var fetch = require('isomorphic-fetch');
 
 module.exports = function(gds) {
     gds.on('saved', function(fileName) {
-        fetch('https://files.holderdeord.no/gdrive/' + fileName + '.json', {
-            method: 'PURGE'
-        }).then(function(res) {
-            console.log('purge', fileName, res.status, res.statusText);
-        });
+        // fetch('https://files.holderdeord.no/gdrive/' + fileName + '.json', {
+        //     method: 'PURGE'
+        // }).then(function(res) {
+        //     console.log('purge', fileName, res.status, res.statusText);
+        // });
     });
 
     gds.on('synced', function(state) {
