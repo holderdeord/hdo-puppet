ea458d1221915ba4744f4ba042841485aa092ce5
Jari Bakken <jari@holderdeord.no>
2017-08-08 13:08:20 +0200

    Move promise dump cron job to hdo user

--- modules/hdo/manifests/webapp/exporter.pp
+++ modules/hdo/manifests/webapp/exporter.pp
@@ -22,9 +22,19 @@ class hdo::webapp::exporter(
   }
 
   cron { 'data-export-promises':
-    ensure      => $ensure,
+    ensure      => absent,
     command     => "bash -l -c 'cd ${hdo::params::app_root} && bundle exec script/dump-promises.sh > /webapps/files/data/csv/promises.csv'",
     user        => postgres,
+    environment => ["RAILS_ENV=${hdo::params::environment}", 'PATH=/usr/local/bin:/usr/bin:/bin', "MAILTO=${hdo::params::admin_email}"],
+    require     => Class['hdo::webapp'],
+    hour        => $hour,
+    minute      => $minute
+  }
+
+  cron { 'data-export-promises':
+    ensure      => $ensure,
+    command     => "bash -l -c 'cd ${hdo::params::app_root} && bundle exec script/dump-promises.sh > /webapps/files/data/csv/promises.csv'",
+    user        => hdo,
     environment => ["RAILS_ENV=${hdo::params::environment}", 'PATH=/usr/local/bin:/usr/bin:/bin', "MAILTO=${hdo::params::admin_email}"],
     require     => Class['hdo::webapp'],
     hour        => $hour,
